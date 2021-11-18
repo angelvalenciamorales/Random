5af4ddadcd52afc94071db5f686aea3d6bf55647
unknown <21340736@itnogales.edu.mx>
2021-11-18 07:03:47 +0100

    Tarea

---
+++ Random/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Random
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(391, 361);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(202, 65);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 487);
+            this.Controls.Add(this.btnRandom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandom;
+    }
+}
+
---
+++ Random/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime;
+using System.Windows.Forms;
+
+namespace Random
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+
+            
+           
+        var random = new System.Random();
+           // int n = random.Next(10);
+            //MessageBox.Show(n.ToString());
+            int[,] en = new int[100,100];
+
+            for(int i = 0; i < 100; i++)
+                
+            {
+                
+                for(int x = 0; x < 100; x++)
+                {
+                    en[x,i] = random.Next(500, 5000);
+                }
+            }
+                MessageBox.Show("Se terminaron los datos:");
+
+        }
+        
+    }
+}
